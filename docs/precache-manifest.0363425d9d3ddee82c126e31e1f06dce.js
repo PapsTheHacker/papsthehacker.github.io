892c442c39a81f8045135c175b4402389353c587
Promise <papsthehacker@gmail.com>
2021-07-12 15:48:16 +0000

    initial deploy

---
+++ docs/precache-manifest.0363425d9d3ddee82c126e31e1f06dce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70df595d0d3a1a5b26f129dc71bbe0c2",
+    "url": "/Hero Person.png"
+  },
+  {
+    "revision": "8f77586bbf261f185032",
+    "url": "/css/app.d3a27a3b.css"
+  },
+  {
+    "revision": "20c81a504013bbfd6b28e9430cb62799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "807fa7c3e916df8c2fbe",
+    "url": "/js/about.e8449b60.js"
+  },
+  {
+    "revision": "8f77586bbf261f185032",
+    "url": "/js/app.7deb6c22.js"
+  },
+  {
+    "revision": "e40427a63768ea4a170d",
+    "url": "/js/chunk-vendors.e107f5c6.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "5ee2b87b1a6b827e43176714e0448459",
+    "url": "/promise-as-hero.png"
+  },
+  {
+    "revision": "51fb2c41c6cf216679aa708c2ca40171",
+    "url": "/promise-as-logo.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a777fa6fe28ed5e1e1c1abfd22f76a24",
+    "url": "/strategies.svg"
+  }
+]);
