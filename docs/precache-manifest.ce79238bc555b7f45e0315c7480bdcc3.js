d60d5b333c9d456eecf98ee017b36b7fbdcb0550
Promise <papsthehacker@gmail.com>
2021-07-19 08:30:13 +0000

    modify responsive breakpoints

---
+++ docs/precache-manifest.ce79238bc555b7f45e0315c7480bdcc3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70df595d0d3a1a5b26f129dc71bbe0c2",
+    "url": "/Hero Person.png"
+  },
+  {
+    "revision": "fd77d322e77429d76941",
+    "url": "/css/app.a9bca7f5.css"
+  },
+  {
+    "revision": "859858d2109d53a006c1ffdd22b6550e",
+    "url": "/images/brands/dribbble.svg"
+  },
+  {
+    "revision": "48a3232b44d5ac5cc40f44ae6941373d",
+    "url": "/images/brands/slack.svg"
+  },
+  {
+    "revision": "732112794ea4a3fde47f627fa88d928b",
+    "url": "/images/brands/webflow.svg"
+  },
+  {
+    "revision": "57bf780e061c230a09cc73b8d76c4700",
+    "url": "/images/brands/youtube.svg"
+  },
+  {
+    "revision": "1df5ae3ab798bb406e4b31c3cc6c077c",
+    "url": "/images/projects/project-sample-1.png"
+  },
+  {
+    "revision": "7891ffc3a4d0c2e7f67045df492c7882",
+    "url": "/images/projects/project-sample-2.png"
+  },
+  {
+    "revision": "a94d4f0568a9e8f7d0b832752df07f9b",
+    "url": "/images/projects/project-sample-3.png"
+  },
+  {
+    "revision": "e0c94ae84432cb08cf8f9b6449a76500",
+    "url": "/images/projects/project-sample-4.png"
+  },
+  {
+    "revision": "ea503876fb3e54f749c595b634bd7b8a",
+    "url": "/images/projects/project-sample-5.png"
+  },
+  {
+    "revision": "0c668f9f8281cdb17a24eb1b621462fa",
+    "url": "/images/projects/project-sample-6.png"
+  },
+  {
+    "revision": "9edcd9865e15d1eaea430f8362eb0fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1109e9490ff6c23e3aa1",
+    "url": "/js/about.b2adc6fe.js"
+  },
+  {
+    "revision": "fd77d322e77429d76941",
+    "url": "/js/app.836326f8.js"
+  },
+  {
+    "revision": "ee25d71da8af9b1f9e33",
+    "url": "/js/chunk-vendors.8360cba8.js"
+  },
+  {
+    "revision": "38c559f366d41acc5f74",
+    "url": "/js/services.1d396db6.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "5ee2b87b1a6b827e43176714e0448459",
+    "url": "/promise-as-hero.png"
+  },
+  {
+    "revision": "51fb2c41c6cf216679aa708c2ca40171",
+    "url": "/promise-as-logo.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a777fa6fe28ed5e1e1c1abfd22f76a24",
+    "url": "/strategies.svg"
+  }
+]);
